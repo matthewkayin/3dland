f3db1b3a2f179b6dd1a1dc28597a2e76ac987e95
Matthew Madden <matthewkayin@gmail.com>
2023-01-29 09:48:21 -0800

    getting somewhere
    Signed-off-by: Matthew Madden <matthewkayin@gmail.com>

--- player.gd
+++ player.gd
@@ -8,7 +8,7 @@ onready var arrow3 = $arrow3
 
 const MAX_SPEED = 10
 const ACCELERATION_MAGNITUDE = 2
-const MAX_ACCELERATION_ANGLE = 2
+const MAX_ACCELERATION_ANGLE = 3
 const MIN_ACCELERATION_ANGLE = 0.5
 const MAX_DECELERATION = 1
 
@@ -23,42 +23,38 @@ func _ready():
 func _physics_process(delta):
     var speed_percent = (velocity.length() / MAX_SPEED)
 
-    var rotation_angle = MIN_ACCELERATION_ANGLE + ((1 - speed_percent) * MAX_ACCELERATION_ANGLE)
+    var rotation_speed = 0.1 + ((1 - speed_percent) * MAX_ACCELERATION_ANGLE)
+    # var rotation_speed = MAX_ACCELERATION_ANGLE
     if Input.is_action_pressed("left"):
-        direction = direction.rotated(Vector3.UP, deg2rad(rotation_angle))
+        direction = direction.rotated(Vector3.UP, deg2rad(rotation_speed))
     if Input.is_action_pressed("right"):
-        direction = direction.rotated(Vector3.UP, deg2rad(-rotation_angle))
+        direction = direction.rotated(Vector3.UP, deg2rad(-rotation_speed))
 
     if Input.is_action_pressed("forward"):
-        acceleration_magnitude += 1.0 * delta
+        if speed_percent < 0.3:
+            acceleration_magnitude = ACCELERATION_MAGNITUDE * 0.5
+        elif speed_percent < 0.8:
+            acceleration_magnitude = ACCELERATION_MAGNITUDE
+        else:
+            acceleration_magnitude = ACCELERATION_MAGNITUDE * 0.5
     else:
-        acceleration_magnitude -= 1.0 * delta
+        acceleration_magnitude = 0
     acceleration_magnitude = clamp(acceleration_magnitude, 0, ACCELERATION_MAGNITUDE)
     acceleration = direction * acceleration_magnitude
-    print(acceleration_magnitude)
-
-    var direction2d = Vector2(direction.x, direction.z)
-    var velocity2d = Vector2(velocity.x, velocity.z)
-    var opposite_car_direction_angle = PI / 2
-    if velocity2d.angle_to(direction2d.rotated(PI / 2)) > velocity2d.angle_to(direction2d.rotated(-PI / 2)):
-        opposite_car_direction_angle = -PI / 2
-    var opposite_car_velocity = velocity2d.dot(direction2d.rotated(opposite_car_direction_angle)) / direction2d.length()
-    var opposite_counter_force_2d = direction2d.rotated(opposite_car_direction_angle + PI) * (opposite_car_velocity * 1.0)
-    var opposite_counter_force = Vector3(opposite_counter_force_2d.x, 0, opposite_counter_force_2d.y)
-
-    arrow.look_at(transform.origin - direction, Vector3.UP)
-    arrow2.look_at(transform.origin - velocity.normalized(), Vector3.UP)
-    arrow3.look_at(transform.origin - opposite_counter_force.normalized(), Vector3.UP)
 
     var deceleration = velocity.normalized().rotated(Vector3.UP, PI) * (speed_percent * MAX_DECELERATION)
 
-    var brake = Vector3.ZERO
-    if Input.is_action_pressed("back"):
-        brake = velocity.normalized().rotated(Vector3.UP, PI) * (speed_percent * 10)
-    if velocity == Vector3.ZERO and Input.is_action_pressed("back"):
-        acceleration = Vector3.ZERO
+    # arrow.look_at(transform.origin - direction, Vector3.UP)
+    # arrow2.look_at(transform.origin - velocity.normalized(), Vector3.UP)
 
-    velocity += (acceleration + deceleration + brake + opposite_counter_force) * delta
+    velocity += (acceleration + deceleration) * delta
+
+    var drift_angle = direction.angle_to(velocity)
+    var drift_percent = (drift_angle / (PI / 2))
+    var drift_magnitude = velocity.dot(direction.rotated(Vector3.UP, -PI / 2))
+    var traction = direction.rotated(Vector3.UP, PI / 2) * drift_magnitude * drift_percent
+    velocity += traction * delta
+    # arrow3.look_at(transform.origin - traction.normalized(), Vector3.UP)
 
     if velocity.length() > MAX_SPEED:
         velocity = velocity.normalized() * MAX_SPEED
@@ -68,3 +64,5 @@ func _physics_process(delta):
     var angle = rad2deg(camera.look_direction.signed_angle_to(direction, Vector3.UP))
     sprite.flip_h = angle > 0
     sprite.frame = int(abs(angle) / 15) 
+    if abs(angle) > 5 and sprite.frame == 0:
+        sprite.frame = 1
